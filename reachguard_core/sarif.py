be64075ff35f39509d750bc917fad405d120f48e
Chaitanya Ajay Bhujbal <chaitanyabhujbal912006@gmail.com>
2026-07-25 19:32:37 +0530

    bump: release v0.2.0

--- reachguard_core/sarif.py
+++ reachguard_core/sarif.py
@@ -78,7 +78,7 @@ def generate_sarif(findings: list[Finding], requirements_path: str = "requiremen
                 "tool": {
                     "driver": {
                         "name": "ReachGuard",
-                        "semanticVersion": "0.1.4",
+                        "semanticVersion": "0.2.0",
                         "informationUri": "https://github.com/chaitanyabhujbal912006-afk/reachguard",
                         "rules": rules
                     }
